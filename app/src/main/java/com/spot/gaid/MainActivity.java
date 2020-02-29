a0755b0a046bbcdc79d42511d19019b9fec125c0
allan <lnov.sergey@gmail.com>
2020-02-29 11:18:02 +0300

    Change realization of async response

--- app/src/main/java/com/spot/gaid/MainActivity.java
+++ app/src/main/java/com/spot/gaid/MainActivity.java
@@ -2,6 +2,7 @@ package com.spot.gaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -12,9 +13,9 @@ import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.common.GooglePlayServicesRepairableException;
 
 
-public class MainActivity extends AppCompatActivity implements AsyncResponse {
+public class MainActivity extends AppCompatActivity {
 
-    MyAsyncTask asyncTask =new MyAsyncTask();
+    MyAsyncTask asyncTask = new MyAsyncTask();
     String GAID = null;
 
     @Override
@@ -22,38 +23,34 @@ public class MainActivity extends AppCompatActivity implements AsyncResponse {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        asyncTask.delegate = this;
+        final Context context = this;
+        asyncTask.delegate = new AsyncResponse() {
+            @Override
+            public void processFinish(String output) {
+                //Here you will receive the result fired from async class
+                //of onPostExecute(result) method.
+
+                GAID = output;
+
+                TextView gaid = findViewById(R.id.gaidNum);
+                String g = "GAID = "+ GAID;
+                gaid.setText(g);
+
+                TextView appid = findViewById(R.id.appsNum);
+                String appsFlyerId = AppsFlyerLib.getInstance().getAppsFlyerUID(context);
+                String a = "AppID = "+appsFlyerId;
+                appid.setText(a);
+
+
+                TextView w = findViewById(R.id.web);
+                String URL = "WebUrl = "+"https://link&apid="+appsFlyerId +"&gaid="+GAID;
+                w.setText(URL);
+
+                // Задача в том чтобы перенести код со строчки 38-50 в метод onCreate
+                // Если это возможно
+            }
+        };
         asyncTask.execute();
-    }
-
-    @Override
-    public void processFinish(String output){
-        //Here you will receive the result fired from async class
-        //of onPostExecute(result) method.
-
-
-        GAID = output;
-
-        TextView gaid = findViewById(R.id.gaidNum);
-        String g = "GAID = "+ GAID;
-        gaid.setText(g);
-
-        TextView appid = findViewById(R.id.appsNum);
-        String appsFlyerId = AppsFlyerLib.getInstance().getAppsFlyerUID(this);
-        String a = "AppID = "+appsFlyerId;
-        appid.setText(a);
-
-
-        TextView w = findViewById(R.id.web);
-        String URL = "WebUrl = "+"https://link&apid="+appsFlyerId +"&gaid="+GAID;
-        w.setText(URL);
-
-        // Задача в том чтобы перенести код со строчки 38-50 в метод onCreate
-        // Если это возможно
-
-
-
-
     }
 
     public class MyAsyncTask extends  AsyncTask<Void, Void, String> {
